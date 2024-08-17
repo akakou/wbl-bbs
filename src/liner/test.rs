9bc9beb9504d2db1d0f2597722e7c60f2ede08ed
akakou <akakou571@gmail.com>
2024-08-17 23:29:10 +0900

    test: the commit eq

--- src/liner/test.rs
+++ src/liner/test.rs
@@ -4,7 +4,6 @@ use std::vec;
 use proof::Proof;
 use snowbridge_amcl::{bls381::{big::Big, ecp2::ECP2, hash_to_curve::{self, hash, HashAlgorithm}, mpin::SHA256}, rand::RAND};
 use statement::Statement;
-use utils::{calc_inner_product, calc_inner_product_one};
 use witeness::Witness;
 
 use super::*;
@@ -81,4 +80,52 @@ fn test_dlog() {
 
     res.expect("verification failed")
     
+}
+
+#[test]
+fn test_commit_eq() {
+    let mut rng = RAND::new();
+    let seed = vec![0 as u8, 1,2,3,4,5,6,7,8,9];
+    rng.seed(10, &seed);
+
+    let x = Big::random(&mut rng);
+    let r0 = Big::random(&mut rng);
+    let r1 = Big::random(&mut rng);
+
+    let g0 = ECP2::generator().mul(&Big::random(&mut rng));
+    let g1 = ECP2::generator().mul(&Big::random(&mut rng));
+    
+    let h0 = ECP2::generator().mul(&Big::random(&mut rng));
+    let h1 = ECP2::generator().mul(&Big::random(&mut rng));
+
+    let mut c0 = g0.mul(&x);
+    let tmp0 = h0.mul(&r0);
+    c0.add(&tmp0);
+
+    let mut c1 = g1.mul(&x);
+    let tmp1 = h1.mul(&r1);
+    c1.add(&tmp1);
+
+    let statement = Statement::new(
+        vec![
+            vec![
+                g0, h0, ECP2::new(), 
+            ],
+            vec![
+                g1, ECP2::new(), h1,
+            ],
+        ],
+        vec![
+            c0, c1
+        ],
+    );
+
+    let witness = Witness(vec![
+        x, r0, r1,
+    ]);
+
+    let proof = Proof::prove(&statement, &witness, &mut rng).expect("proving failed");
+    let res = Proof::verify(&statement, &proof);
+
+    res.expect("verification failed")
 }
