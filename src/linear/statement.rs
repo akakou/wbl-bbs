72f52c80b352d80016d4ea31da5d2c55c42f0e7c
akakou <akakou571@gmail.com>
2024-08-17 23:34:02 +0900

    chore: cargo fmt

--- src/linear/statement.rs
+++ src/linear/statement.rs
@@ -1,6 +1,6 @@
 use snowbridge_amcl::bls381::ecp2::ECP2;
 
-use super::error::LinerProofError;
+use super::error::LinearProofError;
 
 pub struct Statement {
     pub f: Vec<Vec<ECP2>>,
@@ -9,24 +9,29 @@ pub struct Statement {
 
 impl Statement {
     pub fn new(f: Vec<Vec<ECP2>>, x: Vec<ECP2>) -> Self {
-        Statement {f,x}
+        Statement { f, x }
     }
 
-    pub fn well_formed(&self) -> Result<(), LinerProofError> {
+    pub fn well_formed(&self) -> Result<(), LinearProofError> {
         let rowlen = self.f[0].len();
 
         for (i, row) in self.f.iter().enumerate() {
             if row.len() != rowlen {
-                return Err(LinerProofError::StatementFNotWellFormed(row.len(), rowlen, i))
+                return Err(LinearProofError::StatementFNotWellFormed(
+                    row.len(),
+                    rowlen,
+                    i,
+                ));
             }
         }
-        
+
         if self.x.len() != self.f.len() {
-            return Err(LinerProofError::StatementNotWellFormed(self.x.len(), self.f.len()))
+            return Err(LinearProofError::StatementNotWellFormed(
+                self.x.len(),
+                self.f.len(),
+            ));
         }
 
-        return Ok(())
+        return Ok(());
     }
 }
-
-
--- src/linear/witeness.rs
+++ src/linear/witeness.rs
@@ -1,21 +1,22 @@
 use snowbridge_amcl::bls381::big::Big;
 
-use super::{error::LinerProofError, statement::Statement};
+use super::{error::LinearProofError, statement::Statement};
 
-
-pub struct Witness (pub Vec<Big>);
+pub struct Witness(pub Vec<Big>);
 
 impl Witness {
     pub fn new(input: usize) -> Self {
         Witness(vec![Big::new(); input])
     }
 
-    pub fn well_formed(&self, statement: &Statement) -> Result<(), LinerProofError> {
+    pub fn well_formed(&self, statement: &Statement) -> Result<(), LinearProofError> {
         if self.0.len() == statement.f[0].len() {
             return Ok(());
         } else {
-            return Err(LinerProofError::WitnessNotWellFormed(self.0.len(), statement.f[0].len() ));
+            return Err(LinearProofError::WitnessNotWellFormed(
+                self.0.len(),
+                statement.f[0].len(),
+            ));
         }
     }
 }
-
