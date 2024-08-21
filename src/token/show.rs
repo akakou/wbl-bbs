a349c2ea2c9b0fd6181deb7fd22814e2c554d817
akakou <akakou571@gmail.com>
2024-08-21 22:12:43 +0900

    feat: linear proof for showing

--- src/token/show.rs
+++ src/token/show.rs
@@ -17,14 +17,16 @@ pub struct BBSShowing {
     pub abar: ECP2,
     pub d: ECP2,
     // pub attributes: Vec<ECP2>,
-    pub tiket: ECP2,
+    pub ticket: ECP2,
     pub commit: ECP2,
+    pub k_commit: ECP2,
 }
 
 pub struct BBSShowingSecret {
-    pub k_commit: ECP2,
     pub k_open: Big,
     pub k_sc: Big,
+    pub r1: Big,
+    pub r2: Big,
 }
 
 impl BBSShowing {
@@ -41,7 +43,7 @@ impl BBSShowing {
 
         let r1 = Big::random(rng);
         let aprime = token.a.mul(&r1);
-        let commit: ECP2 = token.compute_commit_from_token(params);
+        let commit = token.compute_commit_from_token(params);
 
         let mut tmp0 = token.a.mul(&token.e);
         tmp0.neg();
@@ -76,13 +78,15 @@ impl BBSShowing {
                 abar,
                 d,
                 // attributes:
-                tiket: ticket,
+                ticket,
+                k_commit,
                 commit,
             },
             BBSShowingSecret {
-                k_commit,
                 k_open,
                 k_sc,
+                r1,
+                r2,
             },
         ));
     }
@@ -116,13 +120,14 @@ impl BoundShowing {
     }
 
     pub fn show(
+        bbs_showing: &BBSShowing,
         secret: &BBSShowingSecret,
         bit_limit: u8,
         params: &Parameters,
         rng: &mut RAND,
     ) -> Result<Self, ()> {
         let proof = bound::proof::Proof::prove(
-            &secret.k_commit,
+            &bbs_showing.k_commit,
             &bound::Parameters {
                 g: params.h0.clone(),
                 h: params.h1.clone(),
@@ -141,9 +146,14 @@ impl BoundShowing {
         }
     }
 
-    pub fn verify(&self, k_commit: &ECP2, bit_limit: u8, params: &Parameters) -> Result<(), ()> {
+    pub fn verify(
+        &self,
+        bbs_showing: &BBSShowing,
+        bit_limit: u8,
+        params: &Parameters,
+    ) -> Result<(), ()> {
         match self.bound.verify(
-            k_commit,
+            &bbs_showing.k_commit,
             &bound::Parameters {
                 g: params.h0.clone(),
                 h: params.h1.clone(),
@@ -156,3 +166,116 @@ impl BoundShowing {
     }
 }
 
+pub struct LinearShowing {
+    pub linear: linear::proof::Proof,
+}
+
+impl LinearShowing {
+    pub fn new(linear: linear::proof::Proof) -> Result<Self, ()> {
+        Ok(Self { linear: linear })
+    }
+
+    pub fn show(
+        bbs: &BBSShowing,
+        secret: &BBSShowingSecret,
+        token: &Token,
+        origin: &ECP2,
+        params: &Parameters,
+        rng: &mut RAND,
+    ) -> Result<Self, ()> {
+        let mut r3 = secret.r1.clone();
+        r3.invmodp(&order());
+
+        let mut sprime = Big::modmul(&secret.r2, &r3, &order());
+        sprime = Big::modneg(&sprime, &order());
+        sprime.add(&token.s);
+
+        let witness = Witness(vec![
+            Big::modneg(&token.e, &order()),
+            secret.r2.clone(),
+            r3,
+            Big::modneg(&sprime, &order()),
+            token.key.clone(),
+            secret.k_sc.clone(),
+            secret.k_open.clone(),
+        ]);
+
+        let stmt = Self::stmt(bbs, origin, params);
+
+        let linear = linear::proof::Proof::prove(&stmt, &witness, rng).expect("prove failed");
+        witness.satisfied(&stmt).expect("witness not satisfied");
+
+        return Self::new(linear);
+    }
+
+    fn stmt(bbs_showing: &BBSShowing, origin: &ECP2, params: &Parameters) -> Statement {
+        let mut neg_h0 = params.h0.clone();
+        neg_h0.neg();
+
+        let mut x0 = bbs_showing.d.clone();
+        x0.neg();
+        x0.add(&bbs_showing.abar);
+
+        let mut x1 = params.g0.clone();
+        x1.add(&ECP2::new());
+
+        let stmt = Statement::new(
+            vec![
+                vec![
+                    bbs_showing.aprime.clone(),
+                    params.g1.clone(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                ],
+                vec![
+                    ECP2::new(),
+                    ECP2::new(),
+                    bbs_showing.d.clone(),
+                    params.g1.clone(),
+                    neg_h0,
+                    ECP2::new(),
+                    ECP2::new(),
+                ],
+                vec![
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    params.h0.clone(),
+                    params.h1.clone(),
+                ],
+                vec![
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    ECP2::new(),
+                    bbs_showing.ticket.clone(),
+                    bbs_showing.ticket.clone(),
+                    ECP2::new(),
+                ],
+            ],
+            vec![x0, x1, bbs_showing.k_commit.clone(), origin.clone()],
+        );
+
+        // stmt.well_formed().expect("stmt not well formed");
+        return stmt;
+    }
+
+    pub fn verify(
+        &self,
+        bbs_showing: &BBSShowing,
+        origin: &ECP2,
+        params: &Parameters,
+    ) -> Result<(), ()> {
+        let stmt = Self::stmt(bbs_showing, origin, params);
+
+        match self.linear.verify(&stmt) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(()),
+        }
+    }
+}
