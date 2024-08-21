d78c750ea9944efc3caf5c1c7a385f9378a96fd2
akakou <akakou571@gmail.com>
2024-08-21 20:27:13 +0900

    test: bound proof for showing

--- src/token/test.rs
+++ src/token/test.rs
@@ -6,7 +6,7 @@ use snowbridge_amcl::{
 use super::{
     keygen::{PublicKey, SigningKey},
     param::Parameters,
-    show::BBSShowing,
+    show::{BBSShowing, BoundShowing},
     token::Token,
 };
 
@@ -38,7 +38,29 @@ fn test_bbs_showing() {
     let params = Parameters::debug(&mut rng);
     let t = Token::make(vec![1, 2, 3], &sk, &params, &mut rng);
 
-    let s = BBSShowing::show(&t, &ECP2::generator(), 3, &params, &mut rng).expect("showing failed");
+    let (s, _) =
+        BBSShowing::show(&t, &ECP2::generator(), 3, &params, &mut rng).expect("showing failed");
 
     s.verify(&pk).expect("verification failed");
+}
+
+#[test]
+fn test_bound_showing() {
+    let mut rng = RAND::new();
+    let seed = vec![0 as u8, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    rng.seed(10, &seed);
+
+    let sk = SigningKey::random(&mut rng);
+    let pk = PublicKey::from_signing_key(&sk);
+
+    let params = Parameters::debug(&mut rng);
+    let t = Token::make(vec![1, 2, 3], &sk, &params, &mut rng);
+
+    let (bbs, secret) =
+        BBSShowing::show(&t, &ECP2::generator(), 3, &params, &mut rng).expect("showing failed");
+    let bound = BoundShowing::show(&secret, 3, &params, &mut rng).expect("showing failed");
+
+    bound
+        .verify(&secret.k_commit, 3, &params)
+        .expect("verification failed");
 }
