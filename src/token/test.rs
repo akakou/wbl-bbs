45732809542d58e52df44cb73acc4c16b1627d1f
akakou <akakou571@gmail.com>
2024-08-22 10:04:05 +0900

    test: showing proof

--- src/token/test.rs
+++ src/token/test.rs
@@ -3,8 +3,8 @@ use snowbridge_amcl::{bls381::ecp2::ECP2, rand::RAND};
 use super::{
     keygen::{PublicKey, SigningKey},
     param::Parameters,
-    show::{bbs::BBSShowing, bound::BoundShowing, linear::LinearShowing},
-    token::{self, Token},
+    show::{bbs::BBSShowing, bound::BoundShowing, linear::LinearShowing, Showing},
+    token::Token,
 };
 
 #[test]
@@ -87,4 +87,21 @@ fn test_linear_showing() {
     linear
         .verify(&bbs_showing, &ECP2::generator(), &params)
         .expect("verification failed");
+}
+
+
+#[test]
+fn test_showing() {
+    let mut rng = RAND::new();
+    let seed = vec![0 as u8, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    rng.seed(10, &seed);
+
+    let sk = SigningKey::random(&mut rng);
+    let pk = PublicKey::from_signing_key(&sk);
+
+    let params = Parameters::debug(&mut rng);
+    let t = Token::make(vec![1, 2, 3], &sk, &params, &mut rng);
+
+    let showing = Showing::show(&t, &ECP2::generator(), 3, &params, &mut rng).expect("showing failed");
+    showing.verify(3, &ECP2::generator(), &pk, &params).expect("verification failed");
 }
